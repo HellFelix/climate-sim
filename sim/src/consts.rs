45fa9fc116cbf6e969487c62de8e396e51e0cdcf
Felix Hellborg <felix.hellborg.la@gmail.com>
2025-12-02 16:14:26 +0100

    [SIM] (Update): Add constants for calculations

--- sim/src/consts.rs
+++ sim/src/consts.rs
@@ -14,3 +14,10 @@ pub const SPEEDUP: u32 = 10;
 // Projection consts
 pub const HEIGHT: usize = 51;
 pub const WIDTH: usize = 161; // Should be about PI times larger than HEIGHT!
+pub const HEIGHT_AS_F32: f32 = 51.;
+pub const WIDTH_AS_F32: f32 = 161.;
+
+// radius of the world used as a scaling factor in calculations.
+pub fn world_r() -> f32 {
+    (HEIGHT_AS_F32.powi(2) + WIDTH_AS_F32.powi(2)).sqrt()
+}
