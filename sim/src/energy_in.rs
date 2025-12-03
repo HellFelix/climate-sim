f9d726cb04b4d7e0181f42c07e37bc5861ae9ee1
Raddarin <rasmuspernesten@gmail.com>
2025-12-03 15:35:34 +0100

    Infinity go puff

--- sim/src/energy_in.rs
+++ sim/src/energy_in.rs
@@ -22,26 +22,30 @@ pub fn my_f(point: [f32; 3], zenit: [f32; 3]) -> f32 {
 }
 
 pub fn transmition_f(my: f32) -> f32 {
-    let omega: f32 = 0.98; //(spridnign/(spridning + absorbtion))
-    let tao: f32 = 0.3; //optical depth
-    let t: f32 = (omega * tao) / (2.0 * my);
-    return t + (-tao / my).exp();
+    let omega: f32 = 0.98;
+    let tao: f32 = 0.3;
+    let M_eff: f32 = 1.7; //Airmass
+    let C_diff: f32 = (omega * tao * M_eff) / 2.;
+    return C_diff + (-tao / my).exp();
 }
 
 pub fn flux(my: f32) -> f32 {
     let solar_constant: f32 = 1.3608;
-    let ro: f32 = 0.05; //reflection albedo of the surface
-    let r: f32 = 0.08; //albedo of the atmosfeare
-    return (solar_constant * my * transmition_f(-my)) / (1.0 - ro * r);
+    let ro: f32 = 0.05;
+    let r: f32 = 0.08;
+    return (solar_constant * my * transmition_f(my)) / (1.0 - ro * r);
 }
 
 pub fn flux_pp(xy_sun: [f32; 2]) -> Vec<Vec<f32>> {
     let senit: [f32; 3] = vector_from_cord(xy_sun[0] as usize, xy_sun[1] as usize);
-
     let mut heat_matrix = vec![vec![0.0_f32; WIDTH]; HEIGHT];
+
     for y in 0..HEIGHT {
         for x in 0..WIDTH {
-            let point: [f32; 3] = vector_from_cord(x, y);
+            let mut point: [f32; 3] = vector_from_cord(0, 0);
+
+            point = vector_from_cord(x, y);
+
             let my: f32 = my_f(point, senit);
             let flux_pp: f32 = flux(my);
             heat_matrix[y][x] = flux_pp;
